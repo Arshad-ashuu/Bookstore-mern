11a490306c7396b444a3b360a877c506f5fc821a
Arshad-ashuu <mohammadarshad01474@gmail.com>
2024-04-14 20:07:39 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDbURL ='mongodb+srv://mohammadarshad01474:bookstore0110@bookstore.f9kcnxk.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookstore'
